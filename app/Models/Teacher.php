017493dd7a9ff16a2154b6c31839be382367e9f1
ringinrp <ringinrp21@gmail.com>
2024-04-13 07:40:11 +0700

    update model

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Teacher extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fileable = [
+        'user_id',
+        'is_active'
+    ];
 }
